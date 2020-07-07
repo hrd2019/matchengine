7680111bd700671aa53deb66f093b3ef3b8a2859
createfu <hw2102@outlook.com>
2020-07-07 19:42:20 +0800

    add

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,64 @@
-use std::time::SystemTime;
-use std::collections::HashMap;
+use std::borrow::BorrowMut;
 use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::time::SystemTime;
 
-enum Side{
+enum Side {
     Bid,
     Ask,
 }
 
-struct Odr{
-    pc:f64,
-    qty:f64,
-    side:Side,
-    ts:SystemTime,
-    arcy:i64,
+struct Odr {
+    pc: f64,
+    qty: f64,
+    side: Side,
+    ts: SystemTime,
+    arcy: i64,
 }
 
 type qty = f64;
 
 struct Query {
-    pcs: HashMap<f64, qty>,
-    odrs: HashMap<f64, Vec<Odr>>,
-    pcl:Vec<u64>,
+    pcs: HashMap<String, qty>,
+    odrs: HashMap<String, Box<Vec<Odr>>>,
+    pcl: Vec<u64>,
     side: Side,
     pc: f64,
 }
 
-impl Query{
-    fn new(s:Side) -> Query{
-        Query{
+impl Query {
+    fn new(s: Side) -> Query {
+        Query {
             pcs: HashMap::new(),
-            odrs:HashMap::new(),
-            pcl:vec![],
-            side:s,
-            pc: 0.0
+            odrs: HashMap::new(),
+            pcl: vec![],
+            side: s,
+            pc: 0.0,
         }
     }
 
-    fn insert(&mut self, odr : Odr) {
+    fn insert(&mut self, odr: Odr) {
         let item = self.pcs.entry(odr.pc).or_insert(0.0);
         *item += odr.qty;
 
         let v = (odr.pc * odr.arcy as f64) as u64;
 
-        let item = self.odrs.entry(odr.pc).or_insert(vec![]);
-        *item.push(odr);
+        let item = self
+            .odrs
+            .entry(odr.pc as String)
+            .or_insert(Box::new(vec![]));
+        item.push(odr);
 
         self.pcl.push(v);
 
-        for i in self.pcl.iter(){
+        for i in self.pcl.iter() {
             match self.side {
-                Some(Side::Bid) => {
+                Side::Bid => {
                     self.sort();
-                },
-                Some(Side::Ask) => {
+                }
+                Side::Ask => {
                     self.sort();
-                },
+                }
                 _ => {}
             }
         }
@@ -63,14 +67,13 @@ impl Query{
     fn sort(&mut self) {
         let x = self.pcl.as_mut();
         for i in 1..x.len() {
-            let tmp = a[i];
+            let tmp = x[i];
             let mut j = i;
-            while j > 0 && tmp < a[j - 1] {
-                ;
-                a[j] = a[j - 1];
+            while j > 0 && tmp < x[j - 1] {
+                x[j] = x[j - 1];
                 j -= 1;
             }
-            a[j] = tmp;
+            x[j] = tmp;
         }
     }
 }
@@ -78,5 +81,7 @@ impl Query{
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test1() {}
+    fn test1() {
+        println!("test");
+    }
 }
