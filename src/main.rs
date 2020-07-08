d9310cc362f579e91ba314ba1062729dabba65e0
hrd <bigkite@qq.com>
2020-07-08 22:41:24 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut q = Queue::new(Side::Bid);
 
     let o1 = Odr::new(1.2, 0.45, Side::Bid, 5);
-    let o2 = Odr::new(1.3, 0.45, Side::Bid, 5);
+    let o2 = Odr::new(1.4, 0.45, Side::Bid, 5);
 
     q.insert(o1);
     q.insert(o2);
