4714d3200ddb510501dcdd582d503820448e5d62
createfu <hw2102@outlook.com>
2020-07-08 19:25:49 +0800

    update

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,29 +1,36 @@
 pub mod matcher {
     use matchengine::{Asset, Queue, Side};
+    use std::sync::mpsc;
+    use std::sync::mpsc::{Receiver, Sender};
 
     pub trait Process {
         fn matchdo(&mut self);
         fn settledo(&mut self);
     }
 
-    struct Matcher {
+    struct Matcher<T> {
         curcy_asset: Asset,
         value_asset: Asset,
         queue: Queue,
+        sx: Sender<T>,
+        rx: Receiver<T>,
     }
 
-    impl Matcher {
-        pub fn new(c: Asset, v: Asset) -> Matcher {
+    impl<T> Matcher<T> {
+        pub fn new(c: Asset, v: Asset) -> Matcher<T> {
             let mut queue = Queue::new(Side::Bid);
+            let (sx, rx) = mpsc::channel();
             Matcher {
                 curcy_asset: c,
                 value_asset: v,
                 queue,
+                sx: sx,
+                rx: rx,
             }
         }
     }
 
-    impl Process for Matcher {
+    impl<T> Process for Matcher<T> {
         fn matchdo(&mut self) {
             println!("{:#?}", self.queue);
         }
